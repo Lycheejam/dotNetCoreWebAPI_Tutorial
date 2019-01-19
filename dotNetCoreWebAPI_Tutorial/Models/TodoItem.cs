11952730eb24743a800cca4a3cf5fa2b60079186
lycheejam <lycheejamwow@gmail.com>
2019-01-19 23:34:37 +0900

    add Models TodoContext Class

--- dotNetCoreWebAPI_Tutorial/Models/TodoItem.cs
+++ dotNetCoreWebAPI_Tutorial/Models/TodoItem.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace dotNetCoreWebAPI_Tutorial.Models {
+﻿namespace dotNetCoreWebAPI_Tutorial.Models {
     public class TodoItem {
         public long Id { get; set; }
         public string Name { get; set; }
